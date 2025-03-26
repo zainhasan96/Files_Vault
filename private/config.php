d6f11c79564d0e69eec4f70df1efdae8c9d8b135
zainhasan96 <z.n0@bk.ru>
2025-03-27 00:19:39 +0300

    Update config.php

--- private/config.php
+++ private/config.php
@@ -4,9 +4,9 @@ if (!defined('ACCESS_CHECK')) {
     die('Direct access not allowed!');
 }
 define('DB_HOST', 'localhost');
-define('DB_USER', 'u2368910_z.n');
-define('DB_PASS', 'qH8aN7nE3lqH1fQ7');
-define('DB_NAME', 'u2368910_upload');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
 define('SITE_URL', 'https://zain-hasan.ru/files/');
 define('STORAGE_PATH', __DIR__ . '/storage/');
 if (session_status() === PHP_SESSION_NONE) {
